06b616f3018e98911f5cfbbc1653b6f71576e3af
ImBonana <eladtairo@gmail.com>
2025-12-18 16:15:09 +0200

    Add LoggerSubsystem to IntakeSubsystem

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/intake/IntakeSubsystem.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/intake/IntakeSubsystem.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.subsystems.intake;
 
+import com.acmerobotics.dashboard.telemetry.MultipleTelemetry;
 import com.arcrobotics.ftclib.command.Command;
 import com.arcrobotics.ftclib.command.RunCommand;
 import com.arcrobotics.ftclib.command.SubsystemBase;
@@ -7,12 +8,18 @@ import com.arcrobotics.ftclib.hardware.motors.MotorEx;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.maps.MotorMap;
+import org.firstinspires.ftc.teamcode.util.DataLogger;
+import org.firstinspires.ftc.teamcode.util.subsystem.LoggerSubsystem;
 
-public class IntakeSubsystem extends SubsystemBase {
+public class IntakeSubsystem extends SubsystemBase implements LoggerSubsystem {
+    private final MultipleTelemetry telemetry;
+    private final DataLogger dataLogger;
 
     private final MotorEx motor;
 
-    public IntakeSubsystem(HardwareMap hardwareMap) {
+    public IntakeSubsystem(HardwareMap hardwareMap, MultipleTelemetry telemetry, DataLogger dataLogger) {
+        this.telemetry = telemetry;
+        this.dataLogger = dataLogger;
         this.motor = new MotorEx(hardwareMap, MotorMap.INTAKE.getId());
     }
 
@@ -27,6 +34,16 @@ public class IntakeSubsystem extends SubsystemBase {
     public Command stopIntake() {
         return new RunCommand(() -> this.moveMotor(0), this);
 
+    }
+
+    @Override
+    public MultipleTelemetry getTelemetry() {
+        return this.telemetry;
+    }
+
+    @Override
+    public DataLogger getDataLogger() {
+        return this.dataLogger;
     }
 }
 
